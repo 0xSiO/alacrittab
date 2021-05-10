3f33a9bc6bd01ec1b43ca775afe0699492e318df
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-05-10 01:34:17 -0700

    Refactor Notebook tab logic

--- src/notebook.rs
+++ src/notebook.rs
@@ -1,3 +1,4 @@
+use glib::clone;
 use gtk::prelude::*;
 use gtk::{IconSize, Orientation, ReliefStyle, Widget};
 
@@ -12,48 +13,43 @@ impl Notebook {
         }
     }
 
+    fn new_tab(title: &str, close_button: &gtk::Button) -> gtk::Box {
+        let label = gtk::Label::new(Some(title));
+        let tab = gtk::Box::new(Orientation::Horizontal, 0);
+        tab.set_center_widget(Some(&label));
+        tab.pack_end(close_button, false, false, 0);
+        tab.set_hexpand(true);
+        tab.show_all();
+        tab
+    }
+
     pub fn add_tab(&self, title: &str, widget: impl IsA<Widget>) {
         // Close button
-        let button = gtk::Button::new();
-        button.set_relief(ReliefStyle::None);
+        let close_button = gtk::Button::new();
+        close_button.set_relief(ReliefStyle::None);
 
         let close_image =
             gtk::Image::from_icon_name(Some("window-close-symbolic"), IconSize::Button);
-        button.add(&close_image);
-
-        // Label
-        let label = gtk::Label::new(Some(title));
+        close_button.add(&close_image);
 
         // Tab
-        let tab = gtk::Box::new(Orientation::Horizontal, 0);
-        tab.set_center_widget(Some(&label));
-        tab.pack_end(&button, false, false, 0);
-        tab.set_hexpand(true);
-        tab.show_all();
-
-        self.inner.append_page(&widget, Some(&tab));
+        let tab = Self::new_tab(title, &close_button);
+        let page_num = self.inner.append_page(&widget, Some(&tab));
         self.inner.set_tab_reorderable(&widget, true);
 
         // Callbacks
-        button.connect_enter_notify_event(
-            glib::clone!(@weak close_image => @default-return Inhibit(false), move |_, _| {
-                close_image.set_from_icon_name(Some("window-close"), IconSize::Button);
-                Inhibit(false)
-            }),
-        );
+        close_button.connect_enter_notify_event(clone!(@strong close_image => move |_, _| {
+            close_image.set_from_icon_name(Some("window-close"), IconSize::Button);
+            Inhibit(false)
+        }));
 
-        button.connect_leave_notify_event(
-            glib::clone!(@weak close_image => @default-return Inhibit(false), move |_, _| {
-                close_image.set_from_icon_name(Some("window-close-symbolic"), IconSize::Button);
-                Inhibit(false)
-            }),
-        );
+        close_button.connect_leave_notify_event(clone!(@strong close_image => move |_, _| {
+            close_image.set_from_icon_name(Some("window-close-symbolic"), IconSize::Button);
+            Inhibit(false)
+        }));
 
-        button.connect_clicked(glib::clone!(@weak self.inner as notebook => move |_| {
-            let index = notebook
-                .page_num(&widget)
-                .unwrap();
-            notebook.remove_page(Some(index));
+        close_button.connect_clicked(clone!(@strong self.inner as notebook => move |_| {
+            notebook.remove_page(Some(page_num));
         }));
     }
 }
