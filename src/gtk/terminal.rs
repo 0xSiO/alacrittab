9f64bc9f0ea189e7da0ec1d9750ddf042615cfcd
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-05-29 16:44:12 -0700

    Manually initialize Terminal component instead of using proc macro

--- src/gtk/terminal.rs
+++ src/gtk/terminal.rs
@@ -7,24 +7,43 @@ pub enum TerminalMsg {}
 
 pub struct TerminalParams {}
 
-pub struct TerminalModel {}
+pub struct TerminalModel {
+    relm: Relm<Terminal>,
+}
 
-#[widget]
+pub struct Terminal {
+    model: TerminalModel,
+    // TODO: Make a wrapper around gtk::GLArea and implement Draw, then make this a TerminalDisplay
+    display: gtk::GLArea,
+}
+
+impl Update for Terminal {
+    type Model = TerminalModel;
+    type ModelParam = TerminalParams;
+    type Msg = TerminalMsg;
+
+    fn model(relm: &Relm<Self>, _param: Self::ModelParam) -> Self::Model {
+        TerminalModel { relm: relm.clone() }
+    }
+
+    fn update(&mut self, _event: Self::Msg) {}
+}
+
 impl Widget for Terminal {
-    fn model(_relm: &Relm<Self>, _params: TerminalParams) -> TerminalModel {
-        TerminalModel {}
+    type Root = gtk::GLArea;
+
+    fn root(&self) -> Self::Root {
+        self.display.clone()
     }
 
-    fn update(&mut self, event: TerminalMsg) {
-        match event {}
-    }
-
-    view! {
-        #[name = "gl_area"]
-        gtk::GLArea {}
+    fn view(_relm: &Relm<Self>, model: Self::Model) -> Self {
+        Self {
+            model,
+            display: gtk::GLArea::new(),
+        }
     }
 
     fn init_view(&mut self) {
-        self.widgets.gl_area.show_all();
+        self.display.show_all();
     }
 }
