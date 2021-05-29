0de4c2f2bd4514dda324dcd34c311eecba3f1436
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-05-29 16:18:18 -0700

    Add concept of generic Renderer

--- src/common.rs
+++ src/common.rs
@@ -1,17 +1,21 @@
 use alacritty_terminal::term::SizeInfo;
 
+// TODO: Methods needed for drawing to surface
 pub trait Draw {}
 
+// TODO: Methods needed for OpenGL rendering
+pub trait Render {}
+
 #[allow(dead_code)]
-pub struct TerminalDisplay<D: Draw> {
+pub struct TerminalDisplay<R: Render, D: Draw> {
+    renderer: R,
     surface: D,
     size_info: SizeInfo,
-    renderer: (), // TODO
 }
 
 #[allow(dead_code)]
-impl<D: Draw> TerminalDisplay<D> {
-    pub fn new(surface: D, size_info: SizeInfo, renderer: ()) -> Self {
+impl<R: Render, D: Draw> TerminalDisplay<R, D> {
+    pub fn new(surface: D, size_info: SizeInfo, renderer: R) -> Self {
         Self {
             surface,
             size_info,
